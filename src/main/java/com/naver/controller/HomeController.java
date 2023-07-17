45210605d19ed8800665aa103a3e8a07a82589c6
dbswjd07 <10823syn@naver.com>
2023-07-17 14:07:17 +0900

    spring day1

---
+++ src/main/java/com/naver/controller/HomeController.java
@@ -0,0 +1,42 @@
+package com.naver.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller //@Controller 애노테이션을 사용함으로써 인터넷 웹상에서 작동하는 스프링 컨트롤로 인식함
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	//get으로 접근하는 매핑주소로 처리, 매핑주소가 /(루트경로)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		//문자열 속성키이름인 serverTime에 값으로 날짜 포맷정보를 저장 => 키, 값 쌍으로 저장
+		
+		return "home"; //뷰페이지 경로 (뷰리졸브 경로라고도 함) => /WEB-INF/views/home.jsp
+		//이 뷰페이지인 jsp파일명이다.
+	}
+	
+}
